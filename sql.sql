a699520cdfaed57baab86b7281e22c0151c58bf4
Bagher hosseini <bagherhosseini03@gmail.com>
2023-03-18 03:10:23 +0100

    sql queries

--- sql.sql
+++ sql.sql
@@ -1,37 +1,32 @@
 create database todo;
+use todo;
 
 CREATE TABLE users(
 userid int PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR (100) NOT NULL,
 username VARCHAR (100) unique NOT NULL, 
-password VARCHAR (1000) NOT NULL,
-token VARCHAR (1000) NULL
+password VARCHAR (1000) NOT NULL
 );
-
-insert users values
-(0,'bagher', 'bagher', '123');
 
 CREATE TABLE todoList(
 	todoid int PRIMARY KEY AUTO_INCREMENT,
 	username VARCHAR (100) NOT NULL,
 	title VARCHAR (1000) NOT NULL,
-    FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE
+  FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE
 );
-
-insert todolist values
-(0, 'bagher', 'adhslkjdlask');
 
 CREATE TABLE requests(
 	requestid int PRIMARY KEY AUTO_INCREMENT,
 	requestsender VARCHAR (100) NOT NULL,
 	requestreceiver VARCHAR (100) NOT NULL,
-    status int NOT NULL DEFAULT 0,
-    FOREIGN KEY (requestsender) REFERENCES users(username) ON DELETE CASCADE,
-    FOREIGN KEY (requestreceiver) REFERENCES users(username) ON DELETE CASCADE,
-    CONSTRAINT Uniquereq UNIQUE(requestsender, requestreceiver),
+  status int NOT NULL DEFAULT 0,
+  FOREIGN KEY (requestsender) REFERENCES users(username) ON DELETE CASCADE,
+  FOREIGN KEY (requestreceiver) REFERENCES users(username) ON DELETE CASCADE,
+  CONSTRAINT Uniquereq UNIQUE(requestsender, requestreceiver),
 	CONSTRAINT notsameuser CHECK (requestsender <> requestreceiver)
 );
 
+/*kollar om requestsender och requestreceiver inte ska vara samma och tvärtom*/
 DELIMITER //
 CREATE TRIGGER before_insert_requests
     BEFORE INSERT ON requests
@@ -51,6 +46,7 @@ END;
 //
 DELIMITER ;
 
+/*får en user_username sedan hämtar användare som inte har någon request med user_username*/
 DELIMITER //
 CREATE PROCEDURE GetUnrelatedUsers(IN user_username VARCHAR(100))
 BEGIN
@@ -66,11 +62,7 @@ BEGIN
 END //
 DELIMITER ;
 
-CALL GetUnrelatedUsers('bagher1');
-
-insert requests values
-(0, 'bagher', 'bagher1', 1);
-
+/*får en user_username sedan hämtar andra användare som har request med user_username*/
 DELIMITER //
 CREATE PROCEDURE GetRelatedUsers(IN user_username VARCHAR(100))
 BEGIN
@@ -84,9 +76,10 @@ BEGIN
 END //
 DELIMITER ;
 
-CALL GetRelatedUsers('bagher1');
-
-
+/*Får en user_username och friend_username 
+sedan kollar den om friend_username finns 
+efter kollar om user_username och friend_username är vänner
+då hämtar den friend_username todo list*/
 DELIMITER //
 CREATE PROCEDURE GetFriendTodoList(IN user_username VARCHAR(100), IN friend_username VARCHAR(100))
 BEGIN
@@ -115,9 +108,3 @@ BEGIN
   WHERE t.username = friend_username;
 END //
 DELIMITER ;
-
-CALL GetFriendTodoList('bagher1', 'bagher');
-
-
-DELETE FROM requests;
-SET SQL_SAFE_UPDATES = 0;
